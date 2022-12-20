7dea03139a471c4516ff109f29908d851396d92f
Daniel Buriticá <dani.ferbu@gmail.com>
2022-12-20 18:38:28 -0500

    Implements Stats component

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -3,6 +3,7 @@ import { View, Text, ScrollView } from "react-native";
 import { getPokemonDetailsByIdApi } from "../api/pokemon";
 import Header from "../components/Pokemon/Header";
 import Type from "../components/Pokemon/Type";
+import Stats from "../components/Pokemon/Stats";
 
 export default function Pokemon({
   navigation,
@@ -34,6 +35,7 @@ export default function Pokemon({
         type={pokemon.types[0].type.name}
       />
       <Type types={pokemon.types} />
+      <Stats stats={pokemon.stats} />
     </ScrollView>
   );
 }
